8366040155c5f1ebd0937d96c8b0220bcc0f8426
Piotr Maslanka <piotr.maslanka@henrietta.com.pl>
2017-10-12 15:08:10 +0200

    md2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(packages=find_packages(include=['falcondocs', 'falcondocs.*']),
-      install_requires=["falcon", "six"],
+      install_requires=["falcon", "six", "markdown2"],
       tests_require=[
           "nose", "mock", "coverage"
       ],
